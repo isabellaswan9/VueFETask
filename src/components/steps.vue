38e4f8bc178fcfc0b7ec2d4ac741767af39a8fac
isabellaswan9 <3206616224@qq.com>
2021-08-08 16:02:32 +0800

    编写步骤条steps

---
+++ src/components/steps.vue
@@ -0,0 +1,62 @@
+<template>
+	<div class="steps">
+	<p>part A</p>
+	<div>
+		
+				  <div class="stepsItem">
+					  <div class="icon"></div>
+					  <div class="line"></div>
+				  </div>
+				  <div class="stepsItem">
+				  		<div class="icon"></div>
+				  		<div class="line"></div>
+				  </div>
+				  <div class="stepsItem">
+				  		<div class="icon"></div>
+				  		<div class="line"></div>
+				  </div>
+				  <div class="stepsItem">
+				  		<div class="icon"></div>
+				  		<div class="line"></div>
+				  </div>
+				  <div class="stepsItem">
+				  		<div class="icon"></div>
+				  		<div class="line"></div>
+				  </div>
+				  <div class="stepsItem">
+				  		<div class="icon"></div>
+				  		<div class="line"></div>
+				  </div>
+	</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'steps',
+	}
+</script>
+
+<style>
+	.icon {
+	      width: 13px;
+	      height: 13px;
+	      border-radius: 50%;
+	      background: rgba(226, 226, 226, 1);
+	      position: relative;
+	    }
+	.line{
+	      position: absolute;
+		  background: rgba(226, 226, 226, 1);
+		  left:6px;
+	      width: 2px;
+		  height: 25px;
+	    }
+	.stepsItem{
+		width: 13px;
+		height: 38px;
+	}
+	.steps{
+		width: 15%;
+	}
+</style>
